200d9f3b976ce4ccce4fe0e2207fb90c0570fe16
igorjosiel <igor.ribeiro@rbmweb.com.br>
2021-01-04 10:47:33 -0300

    Aprendendo um pouco sobre navegação com rotas em react

---
+++ navegacao/src/index.js
@@ -0,0 +1,10 @@
+import './index.css';
+
+import Reatc from 'react';
+import ReactDOM from 'react-dom';
+import App from './views/App';
+
+ReactDOM.render(
+    <App></App>,
+    document.getElementById('root')
+)
---
+++ navegacao/src/views/examples/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = props => (
+    <div>
+        <h1>Início</h1>
+        <h2>Bem vindo!</h2>
+    </div>
+)
+
+export default Home;
